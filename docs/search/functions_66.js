bdb0be06975bebc3d7c00dcb9ffc044a00b90b4e
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2013-05-20 17:44:21 -0400

    Deploying Appboy iOS docs from script at May 20 at 05:44pm.

--- docs/search/functions_66.js
+++ docs/search/functions_66.js
@@ -6,6 +6,5 @@ var searchData=
   ['feedbackviewcontrollerpopovercontextcanceltapped_3a',['feedbackViewControllerPopoverContextCancelTapped:',['../protocol_a_b_k_feedback_view_controller_popover_context_delegate-p.html#a018552880f03ede83c90ca675154c018',1,'ABKFeedbackViewControllerPopoverContextDelegate-p']]],
   ['feedbackviewcontrollerpopovercontextfeedbacksent_3a',['feedbackViewControllerPopoverContextFeedbackSent:',['../protocol_a_b_k_feedback_view_controller_popover_context_delegate-p.html#a4a8196551936d22bff8de37e20f97132',1,'ABKFeedbackViewControllerPopoverContextDelegate-p']]],
   ['feedviewcontrollermodalcontextclosetapped_3a',['feedViewControllerModalContextCloseTapped:',['../protocol_a_b_k_feed_view_controller_modal_context_delegate-p.html#abec98cc1d45c10f59dc06d1d22539559',1,'ABKFeedViewControllerModalContextDelegate-p']]],
-  ['feedviewcontrollerpopovercontextclosetapped_3a',['feedViewControllerPopoverContextCloseTapped:',['../protocol_a_b_k_feed_view_controller_popover_context_delegate-p.html#a32bdff25b175ecc87cf721f488580fe6',1,'ABKFeedViewControllerPopoverContextDelegate-p']]],
-  ['firstname',['firstName',['../interface_a_b_k_user.html#a751d4cca44b7bcd9033bd177a9e33e32',1,'ABKUser']]]
+  ['feedviewcontrollerpopovercontextclosetapped_3a',['feedViewControllerPopoverContextCloseTapped:',['../protocol_a_b_k_feed_view_controller_popover_context_delegate-p.html#a32bdff25b175ecc87cf721f488580fe6',1,'ABKFeedViewControllerPopoverContextDelegate-p']]]
 ];
---
+++ docs/search/properties_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['navigationbartitle',['navigationBarTitle',['../interface_a_b_k_feed_view_controller_popover_context.html#a9b8a0eec7590b5321cb0ca85c9f427af',1,'ABKFeedViewControllerPopoverContext']]]
+];
