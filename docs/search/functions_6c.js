580830b933bbff3cb427dca229c1907659925267
Jonathan Hyman <jonathan.hyman@post.harvard.edu>
2013-08-16 19:35:09 -0400

    Deploying Appboy iOS docs from script at Aug 16 at 07:35pm.

--- docs/search/functions_6c.js
+++ docs/search/functions_6c.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['logcustomevent_3a',['logCustomEvent:',['../interface_appboy.html#ad80c39e8c96482a77562a5b1a1d387aa',1,'Appboy']]],
-  ['logpurchase_3apriceincents_3a',['logPurchase:priceInCents:',['../interface_appboy.html#ad2c6b67b2fce5207efaaad6a721e4d82',1,'Appboy']]],
+  ['logpurchase_3aincurrency_3aatprice_3a',['logPurchase:inCurrency:atPrice:',['../interface_appboy.html#a63d8c390bff05f87c7f8f86f2fc0deb6',1,'Appboy']]],
+  ['logpurchase_3apriceincents_3a',['logPurchase:priceInCents:',['../interface_appboy.html#a688b21f1a74bb2d943c93b875f4b4986',1,'Appboy']]],
   ['logsocialshare_3a',['logSocialShare:',['../interface_appboy.html#ae16a3ae4f33d7ae23da740d67e4cd277',1,'Appboy']]]
 ];
