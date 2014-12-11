df02584091432d1b992c5c474394bd16eaeb172d
Wenzhi <wenzhi@appboy.com>
2014-12-11 18:44:47 -0500

    Deploying Appboy iOS docs from script at Dec 11 at 06:44pm.

--- docs/search/functions_72.js
+++ docs/search/functions_72.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['registerapplication_3adidreceiveremotenotification_3a',['registerApplication:didReceiveRemoteNotification:',['../interface_appboy.html#a953f95703dfabac613d466bc52f690ab',1,'Appboy']]],
+  ['registerapplication_3adidreceiveremotenotification_3afetchcompletionhandler_3a',['registerApplication:didReceiveRemoteNotification:fetchCompletionHandler:',['../interface_appboy.html#abe0df88250686670ed0cd73915b8b6bf',1,'Appboy']]],
   ['registerpushtoken_3a',['registerPushToken:',['../interface_appboy.html#a85e336e3dec8aa7fb30376e19ce1678e',1,'Appboy']]],
   ['removefromcustomattributearraywithkey_3avalue_3a',['removeFromCustomAttributeArrayWithKey:value:',['../interface_a_b_k_user.html#ade60374cb566bd3b7ec789d15059c3ae',1,'ABKUser']]],
   ['requestfeedrefresh',['requestFeedRefresh',['../interface_appboy.html#a680549ab7dba8c5971fd9a7fd938b452',1,'Appboy']]],
