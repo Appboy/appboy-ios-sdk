b52ea98babf2f5a28d571fca190c8416460b61c4
Brian Wheeler <briancaw@gmail.com>
2015-05-20 17:19:44 -0400

    Deploying Appboy iOS docs from script at May 20 at 05:19pm.

--- docs/search/functions_6c.js
+++ docs/search/functions_6c.js
@@ -1,11 +1,14 @@
 var searchData=
 [
   ['logcustomevent_3a',['logCustomEvent:',['../interface_appboy.html#ad80c39e8c96482a77562a5b1a1d387aa',1,'Appboy']]],
+  ['logcustomevent_3awithproperties_3a',['logCustomEvent:withProperties:',['../interface_appboy.html#a4f0051d73d85cb37f63c232248124c79',1,'Appboy']]],
   ['logfeedbackdisplayed',['logFeedbackDisplayed',['../interface_appboy.html#a06154478e49f48c168232f48455e8173',1,'Appboy']]],
   ['logfeeddisplayed',['logFeedDisplayed',['../interface_appboy.html#a90f5b55cdf6b2f18511f23f4bd3ce8f2',1,'Appboy']]],
   ['loginappmessageclickedwithbuttonid_3a',['logInAppMessageClickedWithButtonID:',['../interface_a_b_k_in_app_message_immersive.html#a36c52cc221aa193c03adc8bb97f3405c',1,'ABKInAppMessageImmersive']]],
   ['loginappmessageimpression',['logInAppMessageImpression',['../interface_a_b_k_in_app_message.html#adf845c0449a477a75bdaf84e39cb835b',1,'ABKInAppMessage']]],
   ['logpurchase_3aincurrency_3aatprice_3a',['logPurchase:inCurrency:atPrice:',['../interface_appboy.html#a63d8c390bff05f87c7f8f86f2fc0deb6',1,'Appboy']]],
+  ['logpurchase_3aincurrency_3aatprice_3awithproperties_3a',['logPurchase:inCurrency:atPrice:withProperties:',['../interface_appboy.html#ad35bb238aaa4fe9d1ede0439a4c401db',1,'Appboy']]],
   ['logpurchase_3aincurrency_3aatprice_3awithquantity_3a',['logPurchase:inCurrency:atPrice:withQuantity:',['../interface_appboy.html#aa4c66ed060890c68690fd629a62ebfa2',1,'Appboy']]],
+  ['logpurchase_3aincurrency_3aatprice_3awithquantity_3aandproperties_3a',['logPurchase:inCurrency:atPrice:withQuantity:andProperties:',['../interface_appboy.html#ac0d8500d815c33f6fbbf4fcd1e619883',1,'Appboy']]],
   ['logsocialshare_3a',['logSocialShare:',['../interface_appboy.html#ae16a3ae4f33d7ae23da740d67e4cd277',1,'Appboy']]]
 ];
