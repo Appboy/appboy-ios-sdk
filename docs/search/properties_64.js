3ce6d0338542bc64327e57d7076a0ab4779b1c69
Wenzhi <wenzhi@appboy.com>
2016-07-18 19:51:55 -0400

    Deploying Appboy iOS docs from script at Jul 18 at 07:51pm.

--- docs/search/properties_64.js
+++ docs/search/properties_64.js
@@ -3,6 +3,7 @@ var searchData=
   ['dateofbirth',['dateOfBirth',['../interface_a_b_k_user.html#a271442b2725658da79fd2d0de580c2e5',1,'ABKUser']]],
   ['delegate',['delegate',['../interface_a_b_k_in_app_message_controller.html#a80753997e8a3c809d0fa984f7d6d6bbe',1,'ABKInAppMessageController']]],
   ['deprecated_5fattribute',['DEPRECATED_ATTRIBUTE',['../interface_a_b_k_user.html#a136e075a0d2849028e7c7b527e9ccad3',1,'ABKUser']]],
+  ['disablelocationtracking',['disableLocationTracking',['../interface_a_b_k_location_manager.html#a7610c8805b82a90e6eae9b3869b7d265',1,'ABKLocationManager']]],
   ['disableunreadindicator',['disableUnreadIndicator',['../interface_a_b_k_feed_view_controller.html#a8cdecdce9740e996a73379f3722f5f69',1,'ABKFeedViewController::disableUnreadIndicator()'],['../interface_a_b_k_feed_view_controller_generic_context.html#afc475ca9bf5ba4112b91412662c85ac5',1,'ABKFeedViewControllerGenericContext::disableUnreadIndicator()']]],
   ['duration',['duration',['../interface_a_b_k_in_app_message.html#a320737ccedc29526e3f8e4612669326f',1,'ABKInAppMessage']]]
 ];
