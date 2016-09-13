9b7ded8680760fe1da00dce6c4ad174827ec5e9a
Wenzhi <wenzhi@appboy.com>
2016-09-12 20:43:28 -0400

    Deploying Appboy iOS docs from script at Sep 12 at 08:43pm.

--- docs/search/functions_67.js
+++ docs/search/functions_67.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getactionwithidentifier_3aforremotenotification_3acompletionhandler_3a',['getActionWithIdentifier:forRemoteNotification:completionHandler:',['../interface_appboy.html#aaefb476e4d18a4a4712bcce0cbfacc66',1,'Appboy']]],
+  ['getactionwithidentifier_3aforremotenotification_3acompletionhandler_3a',['getActionWithIdentifier:forRemoteNotification:completionHandler:',['../interface_appboy.html#a73d0f5d04ce2f298ca43304ded9e6964',1,'Appboy']]],
   ['getapiendpoint_3a',['getApiEndpoint:',['../protocol_a_b_k_appboy_endpoint_delegate-p.html#a8dd39a61f14febd5a8657f3e69fdbd9e',1,'ABKAppboyEndpointDelegate-p']]],
   ['getcardsincategories_3a',['getCardsInCategories:',['../interface_a_b_k_feed_controller.html#a3757d7cd823b0ac4b83ecf55f7c6a7d2',1,'ABKFeedController']]],
   ['getresourceendpoint_3a',['getResourceEndpoint:',['../protocol_a_b_k_appboy_endpoint_delegate-p.html#a83caa354fcc45bd57b92794964bf3f08',1,'ABKAppboyEndpointDelegate-p']]]
--- docs/search/functions_75.js
+++ docs/search/functions_75.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['unreadcardcountforcategories_3a',['unreadCardCountForCategories:',['../interface_a_b_k_feed_controller.html#a5d3db07ebf01d83aed7ff50de5904dc8',1,'ABKFeedController']]],
-  ['unsetcustomattributewithkey_3a',['unsetCustomAttributeWithKey:',['../interface_a_b_k_user.html#ac7e013d77cb3db42b29e17755c11b91d',1,'ABKUser']]]
+  ['unsetcustomattributewithkey_3a',['unsetCustomAttributeWithKey:',['../interface_a_b_k_user.html#ac7e013d77cb3db42b29e17755c11b91d',1,'ABKUser']]],
+  ['usernotificationcenter_3adidreceivenotificationresponse_3awithcompletionhandler_3a',['userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:',['../interface_appboy.html#a01d9e2d6766a041bb0809c88fb3e1278',1,'Appboy']]]
 ];
