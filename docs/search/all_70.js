d49aed590ccad83b4528ce2e8604e65546d3bae3
Wenzhi <wenzhi@appboy.com>
2016-11-03 16:42:37 -0400

    Deploying Appboy iOS docs from script at Nov 03 at 04:42pm.

--- docs/search/all_70.js
+++ docs/search/all_70.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['phone',['phone',['../interface_a_b_k_user.html#abdf23b4e4c96cd44ae13ce3cffeee99b',1,'ABKUser']]],
+  ['pushauthorizationfromusernotificationcenter_3a',['pushAuthorizationFromUserNotificationCenter:',['../interface_appboy.html#afbcf6edbcf47acf6f03fa4fe02168149',1,'Appboy']]],
   ['pushnotificationwassentfromappboy_3a',['pushNotificationWasSentFromAppboy:',['../interface_appboy.html#a0fa827b56c9afc9f5d7aba9a02b9e9fb',1,'Appboy']]]
 ];
--- docs/search/functions_70.js
+++ docs/search/functions_70.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['pushauthorizationfromusernotificationcenter_3a',['pushAuthorizationFromUserNotificationCenter:',['../interface_appboy.html#afbcf6edbcf47acf6f03fa4fe02168149',1,'Appboy']]],
   ['pushnotificationwassentfromappboy_3a',['pushNotificationWasSentFromAppboy:',['../interface_appboy.html#a0fa827b56c9afc9f5d7aba9a02b9e9fb',1,'Appboy']]]
 ];
