424e44da29d8f0bffc5ed8727976f24b764d56a6
Wenzhi <wenzhi@appboy.com>
2017-10-23 18:38:33 -0400

    Deploying Appboy iOS docs from script at Oct 23 at 06:38pm.

--- docs/search/properties_6c.js
+++ docs/search/properties_6c.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['language',['language',['../interface_a_b_k_user.html#acec238faabee5667abdcc1b90bc15dd6',1,'ABKUser']]],
   ['lastname',['lastName',['../interface_a_b_k_user.html#ad4004972ba0597ab2945406c0a4b3336',1,'ABKUser']]],
   ['lastupdate',['lastUpdate',['../interface_a_b_k_feed_controller.html#ac73559d03f1ad8081f66c34ba8504781',1,'ABKFeedController']]],
   ['locationmanager',['locationManager',['../interface_appboy.html#a942e878042066fc6c3762b98c6e7569a',1,'Appboy']]]
