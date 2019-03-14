26505a9c856cb00ffcf5552b753ef7e1645b8eeb
Chava Friedler <chava.friedler@braze.com>
2019-03-14 14:51:42 -0400

    Deploying Braze iOS docs from script at Mar 14 at 02:51pm.

--- docs/search/all_62.js
+++ docs/search/all_62.js
@@ -4,6 +4,7 @@ var searchData=
   ['beforecontrolmessageimpressionlogged_3a',['beforeControlMessageImpressionLogged:',['../protocol_a_b_k_in_app_message_controller_delegate-p.html#a0e8cc58099bb0798b38af02c7049a789',1,'ABKInAppMessageControllerDelegate-p']]],
   ['beforeinappmessagedisplayed_3a',['beforeInAppMessageDisplayed:',['../protocol_a_b_k_in_app_message_controller_delegate-p.html#a829bd5ba5ca7ce9ec671bf112c030b95',1,'ABKInAppMessageControllerDelegate-p']]],
   ['buttonbackgroundcolor',['buttonBackgroundColor',['../interface_a_b_k_in_app_message_button.html#a631dcbc3fc2014539892b5bea7b72b5b',1,'ABKInAppMessageButton']]],
+  ['buttonbordercolor',['buttonBorderColor',['../interface_a_b_k_in_app_message_button.html#a8d521d68bf10d9a292c2bb0123e4d974',1,'ABKInAppMessageButton']]],
   ['buttonclickactiontype',['buttonClickActionType',['../interface_a_b_k_in_app_message_button.html#aff938abb33df633a3473bdd457f210b0',1,'ABKInAppMessageButton']]],
   ['buttonclickeduri',['buttonClickedURI',['../interface_a_b_k_in_app_message_button.html#a5f38cd1057b3fb113f55252098ad4527',1,'ABKInAppMessageButton']]],
   ['buttonid',['buttonID',['../interface_a_b_k_in_app_message_button.html#a156c7b45dc50181841ac31ab64edde71',1,'ABKInAppMessageButton']]],
--- docs/search/properties_62.js
+++ docs/search/properties_62.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['backgroundcolor',['backgroundColor',['../interface_a_b_k_in_app_message.html#aadcbc0aac4cce6e40c58f7b035a9139e',1,'ABKInAppMessage']]],
   ['buttonbackgroundcolor',['buttonBackgroundColor',['../interface_a_b_k_in_app_message_button.html#a631dcbc3fc2014539892b5bea7b72b5b',1,'ABKInAppMessageButton']]],
+  ['buttonbordercolor',['buttonBorderColor',['../interface_a_b_k_in_app_message_button.html#a8d521d68bf10d9a292c2bb0123e4d974',1,'ABKInAppMessageButton']]],
   ['buttonclickactiontype',['buttonClickActionType',['../interface_a_b_k_in_app_message_button.html#aff938abb33df633a3473bdd457f210b0',1,'ABKInAppMessageButton']]],
   ['buttonclickeduri',['buttonClickedURI',['../interface_a_b_k_in_app_message_button.html#a5f38cd1057b3fb113f55252098ad4527',1,'ABKInAppMessageButton']]],
   ['buttonid',['buttonID',['../interface_a_b_k_in_app_message_button.html#a156c7b45dc50181841ac31ab64edde71',1,'ABKInAppMessageButton']]],
