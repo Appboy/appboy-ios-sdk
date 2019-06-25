bcbdd7af80553a7002a9a7204f5b7c9c8b31af3c
BrandonStalnaker <bstalnaker@mparticle.com>
2019-06-25 14:10:15 -0400

    Cover endpoint override edge cases

--- Example/Stopwatch/OverrideEndpointDelegate.m
+++ Example/Stopwatch/OverrideEndpointDelegate.m
@@ -11,14 +11,18 @@
 }
 
 - (NSString *)getApiEndpoint:(NSString *)appboyApiEndpoint {
-  
-  NSMutableString *modifiedEndpoint = [appboyApiEndpoint mutableCopy];
-  NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:
-                                @"https.*\\.com" options:0 error:nil];
-  
-  [regex replaceMatchesInString:modifiedEndpoint options:0 range:NSMakeRange(0, [modifiedEndpoint length]) withTemplate:self.appboyEndpoint];
-
-  return modifiedEndpoint;
+    NSMutableString *modifiedEndpoint = [appboyApiEndpoint mutableCopy];
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:
+                                  @"https.*\\.com" options:0 error:nil];
+    
+    if (self.appboyEndpoint && modifiedEndpoint) {
+        [regex replaceMatchesInString:modifiedEndpoint options:0 range:NSMakeRange(0, [modifiedEndpoint length]) withTemplate:self.appboyEndpoint];
+        if (![modifiedEndpoint containsString:@"https://"] && ![modifiedEndpoint containsString:@"http://"]) {
+            modifiedEndpoint = [NSMutableString stringWithFormat:@"https://%@", modifiedEndpoint];
+        }
+    }
+    
+    return [modifiedEndpoint copy];
 }
 
 @end
