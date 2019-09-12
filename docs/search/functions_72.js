3aeffb52bdda779cff696aa284160816826bc6ab
Chava Friedler <chava.friedler@braze.com>
2019-09-12 12:54:40 -0400

    Deploying Braze iOS docs from script at Sep 12 at 12:54pm.

--- docs/search/functions_72.js
+++ docs/search/functions_72.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['registerapplication_3adidreceiveremotenotification_3a',['registerApplication:didReceiveRemoteNotification:',['../interface_appboy.html#a430d62354b5cf28c9366537ae8d05b96',1,'Appboy']]],
   ['registerapplication_3adidreceiveremotenotification_3afetchcompletionhandler_3a',['registerApplication:didReceiveRemoteNotification:fetchCompletionHandler:',['../interface_appboy.html#abe0df88250686670ed0cd73915b8b6bf',1,'Appboy']]],
-  ['registerpushtoken_3a',['registerPushToken:',['../interface_appboy.html#a85e336e3dec8aa7fb30376e19ce1678e',1,'Appboy']]],
+  ['registerdevicetoken_3a',['registerDeviceToken:',['../interface_appboy.html#afcdc8ce638ec414503b1f9fce8924e19',1,'Appboy']]],
   ['removefromcustomattributearraywithkey_3avalue_3a',['removeFromCustomAttributeArrayWithKey:value:',['../interface_a_b_k_user.html#ade60374cb566bd3b7ec789d15059c3ae',1,'ABKUser']]],
   ['removelocationcustomattributewithkey_3a',['removeLocationCustomAttributeWithKey:',['../interface_a_b_k_user.html#a01db7758665fd9fa6c82eac9911a1101',1,'ABKUser']]],
   ['requestcontentcardsrefresh',['requestContentCardsRefresh',['../interface_appboy.html#a89a3450fc8d971a16a977d8c5f47dd82',1,'Appboy']]],
