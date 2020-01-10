0df935742845f5a395a04d0abba00e46c729e9e8
Chava Friedler <chava.friedler@braze.com>
2020-01-10 11:01:33 -0500

    Deploying Braze iOS docs from script at Jan 10 at 11:01am.

--- docs/search/all_69.js
+++ docs/search/all_69.js
@@ -25,9 +25,9 @@ var searchData=
   ['isappboyremotenotification_3a',['isAppboyRemoteNotification:',['../interface_a_b_k_push_utils.html#a9510314a1e1f6cf457872505b2af1599',1,'ABKPushUtils']]],
   ['isappboyusernotification_3a',['isAppboyUserNotification:',['../interface_a_b_k_push_utils.html#a014da7e11d2d848bb95411caa989f3e8',1,'ABKPushUtils']]],
   ['iscontrol',['isControl',['../interface_a_b_k_in_app_message.html#a2b718dcc018145b379e58cf7ffff13c3',1,'ABKInAppMessage']]],
-  ['isgeofencessyncremotenotification_3a',['isGeofencesSyncRemoteNotification:',['../interface_a_b_k_push_utils.html#a5ee162b975b49b8ded263dd3ec443e49',1,'ABKPushUtils']]],
-  ['isgeofencessyncusernotification_3a',['isGeofencesSyncUserNotification:',['../interface_a_b_k_push_utils.html#ae0946e7e633ccf921ee4ca6a637b1cca',1,'ABKPushUtils']]],
-  ['ispushstoryremotenotification_3a',['isPushStoryRemoteNotification:',['../interface_a_b_k_push_utils.html#a6031305e43e62216a55b12ca62e85de3',1,'ABKPushUtils']]],
-  ['isuninstalltrackingremotenotification_3a',['isUninstallTrackingRemoteNotification:',['../interface_a_b_k_push_utils.html#a3ea5d7847e40f3784a39d97201ffc7d9',1,'ABKPushUtils']]],
-  ['isuninstalltrackingusernotification_3a',['isUninstallTrackingUserNotification:',['../interface_a_b_k_push_utils.html#a313be6e758a3b3ef3ef35307bc422fc3',1,'ABKPushUtils']]]
+  ['isgeofencessyncremotenotification_3a',['isGeofencesSyncRemoteNotification:',['../interface_a_b_k_push_utils.html#a125929fe98f20625db1a5a71439301a7',1,'ABKPushUtils']]],
+  ['isgeofencessyncusernotification_3a',['isGeofencesSyncUserNotification:',['../interface_a_b_k_push_utils.html#a4b25bef3d7d8cb60a32d08f23ced6811',1,'ABKPushUtils']]],
+  ['ispushstoryremotenotification_3a',['isPushStoryRemoteNotification:',['../interface_a_b_k_push_utils.html#ac193a6e6a991bd95879eee92c9fae069',1,'ABKPushUtils']]],
+  ['isuninstalltrackingremotenotification_3a',['isUninstallTrackingRemoteNotification:',['../interface_a_b_k_push_utils.html#ad1140db01688f422034c33ee3d8b0c47',1,'ABKPushUtils']]],
+  ['isuninstalltrackingusernotification_3a',['isUninstallTrackingUserNotification:',['../interface_a_b_k_push_utils.html#a525becd99865641babef50155171cb8e',1,'ABKPushUtils']]]
 ];
