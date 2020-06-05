548764a4e440f7941382621724d646723f8c9f15
Louis Bur <louisbur92@gmail.com>
2020-06-05 14:40:58 -0400

    Updating Braze SDK

--- AppboyKit/headers/AppboyKitLibrary/Appboy.h
+++ AppboyKit/headers/AppboyKitLibrary/Appboy.h
@@ -13,7 +13,7 @@
 #import <UserNotifications/UserNotifications.h>
 
 #ifndef APPBOY_SDK_VERSION
-#define APPBOY_SDK_VERSION @"3.24.0"
+#define APPBOY_SDK_VERSION @"3.24.1"
 #endif
 
 #if !TARGET_OS_TV
