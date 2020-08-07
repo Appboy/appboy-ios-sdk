01717bbeadfb1ccfeba954c5e7ec8081c1dfbbdb
Daniel Hok <dhok35@gmail.com>
2020-08-07 14:46:30 -0400

    update to use relative path Changelog

--- test_attach_files_to_release.rb
+++ test_attach_files_to_release.rb
@@ -2,18 +2,10 @@
 
 require 'appboy_sdk_release_utils'
 
-# CHANGELOG_FILE_RELATIVE_PATH = "CHANGELOG.md"
+CHANGELOG_RELATIVE_PATH="CHANGELOG.md"
 
-
-puts "Dir.getwd: #{Dir.getwd}"
-
-CHANGELOG_FILE_PATH="#{Dir.getwd}/CHANGELOG.md"
-
-
-puts "CHANGELOG_FILE_PATH: #{CHANGELOG_FILE_PATH}"
 release_utils = AppboySDKReleaseUtils.new()
-new_version_number = release_utils.get_version_number(CHANGELOG_FILE_PATH)
-puts "Extracted the tag to use from CHANGELOG: #{new_version_number}"
+new_version_number = release_utils.get_version_number(CHANGELOG_RELATIVE_PATH)
 
 # Overriding the value for the example
 new_version_number = "hub_test_tag"
