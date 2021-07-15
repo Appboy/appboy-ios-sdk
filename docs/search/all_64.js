d25615eb2ffdd9be4eef2a08d754f66b6e61d151
Louis Bur <louisbur92@gmail.com>
2021-07-15 15:29:53 -0400

    Deploying Braze iOS docs from script at Jul 15 at 03:29pm.

--- docs/search/all_64.js
+++ docs/search/all_64.js
@@ -12,6 +12,7 @@ var searchData=
   ['dismissible',['dismissible',['../interface_a_b_k_content_card.html#a9cc2edfdb70ea07da0fa39b56aae46d7',1,'ABKContentCard']]],
   ['dismissinappmessage_3a',['dismissInAppMessage:',['../interface_a_b_k_in_app_message_immersive_view_controller.html#a6cceb3c849090bb07dbad9dbe2b751a4',1,'ABKInAppMessageImmersiveViewController']]],
   ['displayinappmessageviewwithanimation_3a',['displayInAppMessageViewWithAnimation:',['../interface_a_b_k_in_app_message_window_controller.html#a00f5678a212b1ca5ad11b6a9c5ef84f3',1,'ABKInAppMessageWindowController']]],
-  ['displaynextinappmessagewithdelegate_3a',['displayNextInAppMessageWithDelegate:',['../interface_a_b_k_in_app_message_controller.html#a62183c19ff4e2e0d887ef8e84530a445',1,'ABKInAppMessageController']]],
+  ['displaynextinappmessage',['displayNextInAppMessage',['../interface_a_b_k_in_app_message_controller.html#af2c2a6c950f415b3f0f3017b3d1bac69',1,'ABKInAppMessageController']]],
+  ['displaynextinappmessagewithdelegate_3a',['displayNextInAppMessageWithDelegate:',['../interface_a_b_k_in_app_message_controller.html#a2aec01f9206af487c0fdfde0c119151b',1,'ABKInAppMessageController']]],
   ['duration',['duration',['../interface_a_b_k_in_app_message.html#a320737ccedc29526e3f8e4612669326f',1,'ABKInAppMessage']]]
 ];
--- docs/search/functions_64.js
+++ docs/search/functions_64.js
@@ -5,5 +5,6 @@ var searchData=
   ['disablesdk',['disableSDK',['../interface_appboy.html#a8d3b78a98420713d8590ed63c9172733',1,'Appboy']]],
   ['dismissinappmessage_3a',['dismissInAppMessage:',['../interface_a_b_k_in_app_message_immersive_view_controller.html#a6cceb3c849090bb07dbad9dbe2b751a4',1,'ABKInAppMessageImmersiveViewController']]],
   ['displayinappmessageviewwithanimation_3a',['displayInAppMessageViewWithAnimation:',['../interface_a_b_k_in_app_message_window_controller.html#a00f5678a212b1ca5ad11b6a9c5ef84f3',1,'ABKInAppMessageWindowController']]],
-  ['displaynextinappmessagewithdelegate_3a',['displayNextInAppMessageWithDelegate:',['../interface_a_b_k_in_app_message_controller.html#a62183c19ff4e2e0d887ef8e84530a445',1,'ABKInAppMessageController']]]
+  ['displaynextinappmessage',['displayNextInAppMessage',['../interface_a_b_k_in_app_message_controller.html#af2c2a6c950f415b3f0f3017b3d1bac69',1,'ABKInAppMessageController']]],
+  ['displaynextinappmessagewithdelegate_3a',['displayNextInAppMessageWithDelegate:',['../interface_a_b_k_in_app_message_controller.html#a2aec01f9206af487c0fdfde0c119151b',1,'ABKInAppMessageController']]]
 ];
