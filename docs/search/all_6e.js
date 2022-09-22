8d11d07728332b1c141292ee00d198f244c25e76
Daniel Hok <dhok35@gmail.com>
2022-09-22 13:51:11 -0400

    Deploying Braze iOS docs from script at Sep 22 at 01:51pm.

--- docs/search/all_6e.js
+++ docs/search/all_6e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['newsfeed',['newsFeed',['../interface_a_b_k_news_feed_view_controller.html#ab80b7bbf1e22ea6a6aabee4fc32f887c',1,'ABKNewsFeedViewController']]],
-  ['newsfeedcards',['newsFeedCards',['../interface_a_b_k_feed_controller.html#a0961da8d9ba3663c889c2661ec6dd794',1,'ABKFeedController']]]
+  ['newsfeedcards',['newsFeedCards',['../interface_a_b_k_feed_controller.html#a0961da8d9ba3663c889c2661ec6dd794',1,'ABKFeedController']]],
+  ['nomatchingtriggerforevent_3aname_3a',['noMatchingTriggerForEvent:name:',['../protocol_a_b_k_in_app_message_controller_delegate-p.html#ab4d57b13c51545d487227945a37d4ab8',1,'ABKInAppMessageControllerDelegate-p']]]
 ];
---
+++ docs/search/functions_6e.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['nomatchingtriggerforevent_3aname_3a',['noMatchingTriggerForEvent:name:',['../protocol_a_b_k_in_app_message_controller_delegate-p.html#ab4d57b13c51545d487227945a37d4ab8',1,'ABKInAppMessageControllerDelegate-p']]]
+];
