e9eb9df4eb32b89206849bda5bdcf3ccd36e22c6
Jeriel Ng <jeriel.ng@braze.com>
2022-12-15 10:59:04 -0500

    Deploying Braze iOS docs from script at Dec 15 at 10:59am.

--- docs/search/properties_75.js
+++ docs/search/properties_75.js
@@ -5,6 +5,7 @@ var searchData=
   ['unviewedlineview',['unviewedLineView',['../interface_a_b_k_base_content_card_cell.html#a59a17a044f67c6f3fcc1f01b20391061',1,'ABKBaseContentCardCell']]],
   ['uri',['uri',['../interface_a_b_k_in_app_message.html#a5008f6c7125a3902685d7cfb795539fd',1,'ABKInAppMessage']]],
   ['url',['url',['../interface_a_b_k_content_cards_web_view_controller.html#a8a01cbacb4dc40760b751a23f911cf93',1,'ABKContentCardsWebViewController::url()'],['../interface_a_b_k_feed_web_view_controller.html#a9b2fe0fef42bcf07485fa9c397470114',1,'ABKFeedWebViewController::url()'],['../interface_a_b_k_modal_web_view_controller.html#a42002ca9970f0aa9b08d7e96162b4c54',1,'ABKModalWebViewController::url()']]],
+  ['urlsessionconfiguration',['urlSessionConfiguration',['../interface_appboy.html#a7b6b148d4e2c0d4bca5419e5fa08e746',1,'Appboy']]],
   ['urlstring',['urlString',['../interface_a_b_k_content_card.html#ad5c7d758bc9b579e14bb571a6abab91a',1,'ABKContentCard']]],
   ['user',['user',['../interface_appboy.html#a7cf722b9aba835565b96e5cf1bdd67b1',1,'Appboy']]],
   ['userdescription',['userDescription',['../interface_a_b_k_twitter_user.html#a8290ccfde55e28e82fd0cf4c3e2092d4',1,'ABKTwitterUser']]],
